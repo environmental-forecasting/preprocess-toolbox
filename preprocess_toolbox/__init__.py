920bccc8c23d9c22a18fdd2a5b417b0579e3a82e
James Byrne <jambyr@bas.ac.uk>
2025-05-22 21:26:37 +0100

    Version bump to 0.0.4

--- preprocess_toolbox/__init__.py
+++ preprocess_toolbox/__init__.py
@@ -1,6 +1,7 @@
 """Top-level package for Environmental Forecasting Preprocess Toolbox."""
 
-__author__ = """James Byrne"""
+__author__ = "British Antarctic Survey"
 __copyright__ = "British Antarctic Survey"
 __email__ = 'digitalinnovation@bas.ac.uk'
-__version__ = '0.0.3'
+__license__ = "MIT"
+__version__ = '0.0.4'
